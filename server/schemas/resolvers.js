ff6600c585e9441703bb97374dfefd4c9fe6618a
choelzli <128326139+choelzli@users.noreply.github.com>
2023-09-10 23:23:55 -0400

    export typeDefs and resolvers, define query and mutation functionality, define types

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,75 @@
+const { User, Book } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({
+                    _id: context.userId
+                });
+            }
+            throw AuthenticationError;
+        },
+    },
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email: email });
+
+            if (!user) {
+                throw AuthenticationError;
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create(username, email, password);
+            const token = signToken(user);
+
+            return {token, user };
+        },
+
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+                const user = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: args.book 
+                    }},
+                    { new: true }
+                );
+
+                return user;
+            }
+            throw AuthenticationError;
+        },
+
+        removeBook: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { 
+                        $pull: {
+                        savedBooks: {
+                            bookId: args.deleteBook
+                            },
+                        },
+                    },
+                    { new: true }
+                );
+            }
+            throw AuthenticationError;
+        },
+    },
+};
+
+module.exports = resolvers;
